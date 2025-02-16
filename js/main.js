76abec455c103809cc621b2d166cf4037c196fa9
Prashant Kumar <144902971+Prashant2raja@users.noreply.github.com>
2025-02-16 15:52:28 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ form.addEventListener('submit', async (e) => {
     };
 
     try{
-      const response = await fetch('https://prashantcv.netlify.app//',{
+      const response = await fetch('https://prashantcv.netlify.app/',{
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
