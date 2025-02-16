b3d93b18ec34dafe7ad509190529c88434a8c0ab
Prashant Kumar <144902971+Prashant2raja@users.noreply.github.com>
2025-02-16 18:15:26 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ form.addEventListener('submit', async (e) => {
     };
 
     try{
-      const response = await fetch('https://portfolio-cv-cc64f984f5ce.herokuapp.com/',{
+      const response = await fetch('https://portfolio-cv-42ef80e01032.herokuapp.com/',{
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
